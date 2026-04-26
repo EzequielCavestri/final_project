ad037807662b480a389ed09654fa1d2b0d095443
julianamaidana <julianamaidana03@gmail.com>
2026-04-26 12:39:37 -0300

    Nueva organizacion de trabajo / archivos de cocotb

--- verilog/rtl/tx/prbs9.v
+++ verilog/rtl/tx/prbs9.v
@@ -4,7 +4,7 @@ module prbs9(
   input  wire rst,
   input  wire en,          // avanzar 1 bit
   input  wire [8:0] seed,  // 9 bits, no nulo
-  output wire bit
+  output wire o_bit
 );
   reg [8:0] s;
   wire fb = s[8] ^ s[4];   // x^9 + x^5 + 1
@@ -12,5 +12,5 @@ module prbs9(
     if (rst) s <= (seed!=9'd0) ? seed : 9'h1;
     else if (en) s <= {s[7:0], fb};
   end
-  assign bit = s[8];
+  assign o_bit = s[8];
 endmodule
